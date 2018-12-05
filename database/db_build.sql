2aef3db517ff2906cf8f81ca40bb01ffc817881d
aterna01 <ssaniok90@mail.ru>
2018-12-05 22:00:40 +0000

    data files skeleton

---
+++ database/db_build.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+DROP TABLE IF EXISTS people cascade;
+
+CREATE TABLE people (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100) NOT NULL,
+);
+
+
+  -- DROP TABLE IF EXISTS lunches cascade;
+
+  -- CREATE TABLE lunches (
+  --   id SERIAL PRIMARY KEY NOT NULL,
+  --   lunch VARCHAR(100) NOT NULL,
+  -- );
+  --
+  -- INSERT INTO lunches (lunch) VALUES
+  --   ('Italian'),
+  --   ('Indian'),
+  --   ('Chinese'),
+  --   ('Corbyn');
+  --
+  -- DROP TABLE IF EXISTS bookings cascade;
+  --
+  --   CREATE TABLE bookings (
+  --     id SERIAL PRIMARY KEY NOT NULL,
+  --     PRIMARY KEY VARCHAR(100) NOT NULL,
+  --   );
+
+
+COMMIT;
