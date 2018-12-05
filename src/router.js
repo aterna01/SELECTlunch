e1be8e93750ab932c9fe3e1dc39cb175007d78fb
susanX <susan.gooding@yahoo.co.uk>
2018-12-05 16:19:54 +0000

    router

---
+++ src/router.js
@@ -0,0 +1,23 @@
+const handlers = require("./handlers.js");
+
+const routes = [
+"/css/main.css",
+"/css/reset.css",
+"/js/app.js",
+];
+
+const router = (req, res) => {
+const url = req.url;
+if (url === "/") {
+  handlers.handleHomeRoute(req, res);
+  } else if (routes.includes(url)) {
+  handlers.handlerPublic(req, res, url);
+  } else if (url.indexOf("/data") !== -1) {
+  handlers.handlePartners(req, res);
+  } else {
+  res.writeHead(404, "Content-Type: text/html");
+  res.end("<h1>404 File not found</h1>");
+  }
+};
+
+module.exports = router;
