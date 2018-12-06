64fb10e1f2667e3f468865f1a54ededbb0eb6242
susanX <susan.gooding@yahoo.co.uk>
2018-12-06 16:03:17 +0000

    booking table

--- database/db_build.sql
+++ database/db_build.sql
@@ -3,31 +3,32 @@ BEGIN;
 DROP TABLE IF EXISTS people cascade;
 
 CREATE TABLE people (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(100) NOT NULL
+  people_id SERIAL PRIMARY KEY NOT NULL,
+  name_id VARCHAR(100) NOT NULL
 );
 
+  DROP TABLE IF EXISTS lunches cascade;
+
+  CREATE TABLE lunches (
+    lunch_id SERIAL PRIMARY KEY NOT NULL,
+    lunch VARCHAR(100) NOT NULL
+  );
+
+  INSERT INTO lunches (lunch)
+  VALUES ('Italian'),
+         ('Corbyn'),
+         ('Chinese'),
+         ('Indian');
 
 
-  -- DROP TABLE IF EXISTS lunches cascade;
+  DROP TABLE IF EXISTS bookings cascade;
 
-  -- CREATE TABLE lunches (
-  --   id SERIAL PRIMARY KEY NOT NULL,
-  --   lunch VARCHAR(100) NOT NULL,
-  -- );
-  --
-  -- INSERT INTO lunches (lunch) VALUES
-  --   ('Italian'),
-  --   ('Indian'),
-  --   ('Chinese'),
-  --   ('Corbyn');
-  --
-  -- DROP TABLE IF EXISTS bookings cascade;
-  --
-  --   CREATE TABLE bookings (
-  --     id SERIAL PRIMARY KEY NOT NULL,
-  --     PRIMARY KEY VARCHAR(100) NOT NULL,
-  --   );
-
+  CREATE TABLE bookings (
+    people_id SERIAL REFERENCES people(people_id)NOT NULL,
+    lunch_id SERIAL REFERENCES lunches(lunch_id)NOT NULL,
+    PRIMARY KEY (people_id, lunch_id),
+    vegy BOOLEAN,
+    paid BOOLEAN
+  );
 
 COMMIT;
